14edec0f7800cd9a65a98e2c7043022d42486794
Manhh Nguyenn <ngdmanh806@gmail.com>
2026-06-06 02:04:58 +0700

    Update database.py

--- backend/FastAPI/app/database.py
+++ backend/FastAPI/app/database.py
@@ -3,7 +3,7 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
 # Thông tin chuẩn của Manh
-SQLALCHEMY_DATABASE_URL = "mysql+pymysql://root:manh26042006@localhost:3306/neu_tuyensinh"
+SQLALCHEMY_DATABASE_URL = ""
 
 # SỬA Ở ĐÂY: Phải dùng đúng tên biến SQLALCHEMY_DATABASE_URL
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
